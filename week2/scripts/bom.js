feea6b5bd67bb675ae209fb8464986f86d35dd66
Samuel <spectra.gethelp@gmail.com>
2025-05-14 23:11:35 +0200

    change

--- week2/scripts/bom.js
+++ week2/scripts/bom.js
@@ -1,9 +1,8 @@
-// DOM element references
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-// Ejemplo de cómo preparar un li y botón (aunque aún no se activa)
+
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 
